ae52a3d87062b05d14867628da0f3b0823fda72e
zpadol <zpadol@student.agh.edu.pl>
2026-05-29 15:11:34 +0200

    dodanie DatabaseContext

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ManicureBooking.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
